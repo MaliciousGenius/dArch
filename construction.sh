0bac19b8b7029e1717bf1ecb51848365d6afc628
maliciousgenius <maliciousgenius@gmail.com>
2016-10-24 04:53:25 +0300

    fix

--- construction.sh
+++ construction.sh
@@ -3,3 +3,18 @@
 #
 
 pacman -S --needed base-devel
+
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -si
+cd ..
+
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt
+makepkg -si
+cd ..
+
+git https://aur.archlinux.org/cockpit-git.git
+cd cockpit-git
+makepkg -si
+cd ..
