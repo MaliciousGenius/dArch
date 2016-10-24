53e85d6d5de8007c328bbd2f42a3aeee93fd88b4
maliciousgenius <maliciousgenius@gmail.com>
2016-10-24 23:43:45 +0300

    fix

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2016 Malicious Genius
 #
 
-cat "mothership ansible_connection=local" > /etc/ansible/hosts
+echo "mothership ansible_connection=local" > /etc/ansible/hosts
 
 cd /opt/microarch-custom
 git pull
