b368d114f854d8bcc89d3fea7af3b8eda5eeaac7
maliciousgenius <maliciousgenius@gmail.com>
2016-10-27 02:30:47 +0300

    v.0.1

--- run.sh
+++ run.sh
@@ -2,10 +2,9 @@
 # Copyright (c) 2016 Malicious Genius
 #
 
-echo 'mothership ansible_connection=local' > /etc/ansible/hosts
-
 cd /opt/microarch-custom
 git pull
+echo 'mothership ansible_connection=local' > /etc/ansible/hosts
 ansible-playbook ./site.yml
 
 sleep 5m
