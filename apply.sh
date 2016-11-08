b1faef70c4b0c49a5ecaa218ce029bf05019ef9f
maliciousgenius <maliciousgenius@gmail.com>
2016-11-08 22:18:57 +0300

    v.0.1

--- apply.sh
+++ apply.sh
@@ -9,7 +9,8 @@ if [ ! -f /etc/mothership ]; then
     cd /opt/microarch-custom & git pull
     echo 'mothership ansible_connection=local' > /etc/ansible/hosts
     ansible-playbook ./germination.yml
-    ln -s '/opt/microarch-custom/mothership-startup.service.service' '/etc/systemd/system/multi-user.target.wants/mothership-startup.service.service'
+    ln -s '/opt/microarch-custom/mothership-startup.service.service' '/etc/systemd/system/mothership-startup.service'
+    systemctl daemon-reload
     touch /etc/mothership
 
     sleep 3m
