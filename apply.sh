645e50c7d26c213dd567377a180c7ad05b5ce12f
maliciousgenius <maliciousgenius@gmail.com>
2016-11-18 16:39:45 +0300

    v.0.1

--- apply.sh
+++ apply.sh
@@ -2,15 +2,12 @@
 # Copyright (c) 2016 Malicious Genius
 #
 
-
 #ln -s '/opt/microarch-custom/mothership-startup.service' '/etc/systemd/system/mothership-startup.service'
 #systemctl daemon-reload
 #systemctl enable mothership-startup.service
 
 echo 'mothership ansible_connection=local' > /etc/ansible/hosts
-ansible-playbook ./germination.yml
 cd /opt/microarch-custom & git pull
-cd /opt/microarch-custom
-ansible-playbook ./mothership.yml
-sleep 1m
+ansible-playbook ./playbook/mothership.yml
+sleep 3m
 systemctl reboot
