dc7af4a9058bff9ae08abdda34221e42bf884e01
maliciousgenius <maliciousgenius@gmail.com>
2016-11-19 02:06:20 +0300

    v.0.2

--- prepare.sh
+++ prepare.sh
@@ -4,5 +4,5 @@
 
 echo 'mothership ansible_connection=local' > /etc/ansible/hosts
 cd /opt/microarch-custom & git pull
-ansible-playbook ./playbook/parallels.yml
+ansible-playbook ./playbook/vbox.yml
 
