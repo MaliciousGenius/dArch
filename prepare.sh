0c4c0d4deb6c37fb586509864482f22fef8af7d7
maliciousgenius <maliciousgenius@gmail.com>
2016-11-18 16:30:40 +0300

    v.0.1

--- prepare.sh
+++ prepare.sh
@@ -2,9 +2,6 @@
 # Copyright (c) 2016 Malicious Genius
 #
 
-useradd -m -d /home/system -g sys -G wheel -s /bin/bash system
-echo 'Enter "system" user password'
-passwd system
 cd /opt/microarch-custom
 ansible-playbook ./playbook/parallels.yml
 
