05f7d9cbce6297a7de9c667be32a2a389140292b
maliciousgenius <maliciousgenius@gmail.com>
2016-11-21 19:44:34 +0300

    v.0.2

--- prepare.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2016 Malicious Genius
-#
-
-echo 'mothership ansible_connection=local' > /etc/ansible/hosts
-cd /opt/microarch-custom & git pull
-ansible-playbook ./roles/vmguest/tasks/main.yml
-
