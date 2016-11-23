4312e65beb8473c8b242b1015e75c3b1f020bed5
maliciousgenius <maliciousgenius@gmail.com>
2016-11-23 15:29:42 +0300

    v.0.3

---
+++ microinstaller-chroot.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+# Copyright (c) 2016 Malicious Genius
+#
+
+echo " >> Prepare..."
+
+# Установаю имя хоста
+configure_hostname(){
+    echo mothership > /etc/hostname
+}
+
+# Задаю часовой пояс
+configure_timezone(){
+    ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+}
+
+# Настройка локали
+configure_locale(){
+    cp /etc/locale.gen /etc/locale.gen_bak
+    sed 's/#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen_bak > /etc/locale.gen
+    cp /etc/locale.gen /etc/locale.gen_bak
+    sed 's/#ru_RU.UTF-8/ru_RU.UTF-8/g' /etc/locale.gen_bak > /etc/locale.gen
+    rm /etc/locale.gen_bak
+
+    # Генерация локали
+    locale-gen
+
+    echo 'LANG="ru_RU.UTF-8"' >> /etc/locale.conf
+}
+
+# Настройка русской клавиатуры и шрифта
+select_keymap(){
+    echo 'KEYMAP=ru' >> /etc/vconsole.conf
+    echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
+}
+
+# Создаём новый ramdisk
+configure_mkinitcpio(){
+    mkinitcpio -p linux
+}
+
+# Добавляю загрузчик
+configure_bootloader(){
+    grub-mkconfig -o /boot/grub/grub.cfg
+    grub-install --recheck /dev/sda
+}
+
+# Влючить загрузку dhcpcd
+enable_dhcp(){
+    systemctl enable dhcpcd
+}
+
+# Создаю пользователя admin
+create_admin_user(){
+    groupadd users
+    useradd -m -g users -G wheel -s /bin/bash admin
+}
+
+# Создаю пользователя system
+create_system_user(){
+    groupadd system
+    useradd -m -g system -G wheel -s /bin/bash system
+}
+
+# Настраиваю sudo
+configure_sudo(){
+    cp /etc/sudoers /etc/sudoers.gen_bak
+    sed '/%wheel ALL=(ALL) NOPASSWD: ALL/s/^#//' /etc/sudoers.gen_bak > /etc/sudoers
+    rm /etc/sudoers.gen_bak
+}
+
+# Настраиваю git
+configure_git(){
+    git config --global user.email "maliciousgenius@gmail.com"
+    git config --global user.name "maliciousgenius"
+}
+
+# Загрузка репозитория microarch-custom
+get_repo_microarch(){
+    cd /opt
+    git clone https://github.com/MaliciousGenius/microarch-base.git
+    git clone https://github.com/MaliciousGenius/microarch-custom.git
+}
+
+configure_hostname
+configure_timezone
+configure_locale
+select_keymap
+configure_mkinitcpio
+configure_bootloader
+enable_dhcp
+create_admin_user
+create_system_user
+configure_sudo
+configure_git
+get_repo_microarch
+
+# Установка пароля для пользователя root
+echo 'Enter "root" user password'
+passwd
+
+# Установка пароля для пользователя admin
+echo 'Enter "admin" user password'
+passwd admin
+
+# Установка пароля для пользователя system
+echo 'Enter "system" user password'
+passwd system
+
+pacman -Syu
+
+exit 0
---
+++ microinstaller.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# Copyright (c) 2016 Malicious Genius
+#
+
+# Разбиваю диск на разделы
+partitioning_hd() {
+    parted -s /dev/sda \
+        mklabel msdos \
+        mkpart primary ext2 1M 255M \
+        mkpart primary linux-swap 256M 512M \
+        mkpart primary ext2 513M 100% \
+        set 1 boot on
+}
+
+# Форматирую раделы
+format_partition() {
+    mkfs.ext2 -F -L boot /dev/sda1
+    mkfs.ext4 -F -L root /dev/sda3
+    mkswap /dev/sda2
+}
+
+# Монтирую файловые ситемы
+mount_partition() {
+    mount /dev/sda3 /mnt
+    mkdir -p /mnt/boot
+    mount /dev/sda1 /mnt/boot
+    swapon /dev/sda2
+}
+
+# Устанавливаю набор базовах патетов
+install_packages() {
+    pacstrap /mnt base grub git ansible
+}
+
+# Генерирую fstab
+generation_fstab() {
+    genfstab -U -p /mnt >> /mnt/etc/fstab
+}
+
+# Загружаю сктипт начальных настроек
+get_preparatory_script() {
+    wget https://raw.githubusercontent.com/MaliciousGenius/microarch-base/master/preparatory-script.sh -O /mnt/root/microinstaller-chroot.sh
+    chmod +x /mnt/root/microinstaller-chroot.sh
+}
+
+# Запускаю начальные настройки
+initial_settings() {
+    arch-chroot /mnt /root/microinstaller-chroot.sh
+}
+
+# Удаляю подготовительный сктипт
+erase_preparatory_script() {
+    rm /mnt/root/microinstaller-chroot.sh
+}
+
+echo " >> Installing..."
+
+partitioning_hd
+format_partition
+mount_partition
+install_packages
+generation_fstab
+get_preparatory_script
+initial_settings
+erase_preparatory_script
+
+echo " >> Ok."
+
+exit 0
