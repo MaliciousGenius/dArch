646c92904cea3a7e931bc7bff855788d28a91b8e
maliciousgenius <maliciousgenius@gmail.com>
2016-11-25 10:47:55 +0300

    fix

---
+++ apply_admin.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Copyright (c) 2016 Malicious Genius
+#
+
+ansible-playbook ./desktop-environment.yml
--- roles/vmguest/tasks/files/autostart/60-open-vm-tools.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while true; do
-    vmware-user-suid-wrapper
-    sleep 60
-done
