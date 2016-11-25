f9dcac208461bc2e7a780d9f227b920caea2fb40
maliciousgenius <maliciousgenius@gmail.com>
2016-11-25 10:05:00 +0300

    fix

--- apply.sh
+++ apply.sh
@@ -6,6 +6,7 @@
 #systemctl daemon-reload
 #systemctl enable mothership-startup.service
 
+cp /opt/dArch/roles/base/files/ansible/ansible.cfg /etc/ansible/ansible.cfg
 echo 'mothership ansible_connection=local' > /etc/ansible/hosts
 cd /opt/dArch & git pull
 ansible-playbook ./mothership.yml
