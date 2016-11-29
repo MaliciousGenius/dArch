0bce14f968c9d5811f8194ae39b9cf04703fd7e0
maliciousgenius <maliciousgenius@gmail.com>
2016-11-29 11:42:56 +0300

    v0.2

--- containers.sh
+++ containers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Copyright (c) 2016 Malicious Genius
 #
-docker run -d -p 9000:9000 --privileged -v /var/run/docker.sock:/var/run/docker.sock uifd/ui-for-docker &
+docker run -d -name ui-for-docker -p 9000:9000 --privileged -v /var/run/docker.sock:/var/run/docker.sock uifd/ui-for-dockerr &
